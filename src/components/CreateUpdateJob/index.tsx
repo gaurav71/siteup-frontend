3966fc962240b46c7c0d7714b04e5f3f75be38e9
gaurav <gauravkalyan17@gmail.com>
2021-04-28 21:16:58 +0530

    fix add edit job

--- src/components/CreateUpdateJob/index.tsx
+++ src/components/CreateUpdateJob/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { Modal, Form, Input, InputNumber, Switch, Button, Row, Col, Select } from 'antd'
 import { useCreateSiteUpCheckerJobMutation, useUpdateSiteUpCheckerJobMutation } from '../../generated/graphql'
 import { cacheUpdator } from '../Apollo'
@@ -29,15 +29,16 @@ const CreateUpdateJob: React.FC<CreateUpdateJobProps> = () => {
   const [createMutation, { loading: createLoader, data: createData }] = useCreateSiteUpCheckerJobMutation()
   const [updateMutation, { loading: updateLoader, data: updateData }] = useUpdateSiteUpCheckerJobMutation()
   const { addEditJobModal, setAddEditJobModal, selectedJob } = useDashboardContext()
-  const [cronValue, setCronValue] = useState<string>(initialValues.cron)
-  const [sendMailValue, setSendMailValue] = useState<boolean>(initialValues.sendMailOnFailure)
-  const [customCron, setCustomCron] = useState({ key: '', value: '' })
+  const [cronValue, setCronValue] = useState(initialValues.cron)
+  const [customCron, setCustomCron] = useState({
+    key: '',
+    value: ''
+  })
 
   useEffect(() => {
-    if (addEditJobModal === 'edit' && selectedJob) {
+    if (addEditJobModal === 'edit') {
       form.setFieldsValue(selectedJob)
-      setCronValue(selectedJob.cron)
-      setSendMailValue(selectedJob.sendMailOnFailure)
+      setCronValue((selectedJob as any).cron)
     }
   }, [addEditJobModal])
 
@@ -55,54 +56,36 @@ const CreateUpdateJob: React.FC<CreateUpdateJobProps> = () => {
     }
   }, [updateData])
 
-  useEffect(() => {
-    if (form) {
-      const resetAfterDownCount = sendMailValue
-        ? form.getFieldValue('resetAfterDownCount')
-        : null
-
-      form.setFieldsValue({
-        ...form.getFieldsValue(),
-        cron: cronValue,
-        sendMailOnFailure: sendMailValue,
-        resetAfterDownCount
-      })
-    }
-  }, [cronValue, sendMailValue])
-
   const handleSubmit = async () => {
     try {
       const formData: FormData = await form.validateFields()
-
+      
       if (addEditJobModal === 'add') {
         createMutation({
           variables: formData
         })
-      }
-
-      if (addEditJobModal === 'edit' && selectedJob) {
+      } else {
         updateMutation({
           variables: {
-            jobId: selectedJob._id,
+            jobId: (selectedJob as any)._id,
             ...formData
           }
         })
       }
-    } catch (error) { }
+    } catch(error) {
+      console.log(error)
+    }
   }
 
   const handleCancel = () => {
     form.resetFields()
-    setCronValue(initialValues.cron)
-    setSendMailValue(initialValues.sendMailOnFailure)
     setAddEditJobModal('')
+    setCronValue(initialValues.cron)
   }
 
   const handleCronInputChange = (e: any) => {
     const cronInput = e.target.value
-
     const readableString = getHumanReadableFromCron(cronInput)
-
     const cronAlreadyInDefaults = Object.values(defaultCronOptions).find(({ value }) => value === cronInput)
 
     if (readableString && !cronAlreadyInDefaults) {
@@ -172,44 +155,50 @@ const CreateUpdateJob: React.FC<CreateUpdateJobProps> = () => {
           <Form.Item label="Cron">
             <Row gutter={0}>
               <Col span={10}>
-                <Form.Item
-                  noStyle
-                  name="cron"
-                  rules={[
-                    {
-                      required: true,
-                      message: 'cron is required'
-                    },
-                    {
-                      validator: async (_, value) => {
-                        if (value && !getHumanReadableFromCron(value)) {
-                          throw new Error('Invalid cron')
-                        }
+              <Form.Item
+                noStyle
+                name="cron"
+                rules={[
+                  {
+                    required: true,
+                    message: 'cron is required'
+                  },
+                  {
+                    validator: async (_, value) => {
+                      if (value && !getHumanReadableFromCron(value)) {
+                        throw new Error('Invalid cron')
                       }
                     }
-                  ]}
-                >
-                  <Input
-                    onChange={handleCronInputChange}
-                  />
-                </Form.Item>
+                  }
+                ]}
+              >
+                <Input
+                  onChange={handleCronInputChange}
+                />
+              </Form.Item>
               </Col>
               <Col span={14} >
-                <Select
-                  value={cronValue}
-                  onChange={(val) => setCronValue(val)}
-                >
-                  {Object.values(defaultCronOptions).map((data, index) => (
-                    <Option key={index} value={data.value} >
-                      {data.key}
-                    </Option>
-                  ))}
-                  {customCron.key && (
-                    <Option value={customCron.value} >
-                      {customCron.key}
-                    </Option>
-                  )}
-                </Select>
+              <Select
+                value={cronValue}
+                onChange={(val) => {
+                  form.setFieldsValue({
+                    ...form.getFieldsValue(),
+                    cron: val
+                  })
+                  setCronValue(val)
+                }}
+              >
+                {Object.values(defaultCronOptions).map((data, index) => (
+                  <Option key={index} value={data.value} >
+                    {data.key}
+                  </Option>
+                ))}
+                {customCron.key && (
+                  <Option value={customCron.value} >
+                    {customCron.key}
+                  </Option>
+                )}
+              </Select>
               </Col>
             </Row>
           </Form.Item>
@@ -221,23 +210,40 @@ const CreateUpdateJob: React.FC<CreateUpdateJobProps> = () => {
               required: true
             }]}
           >
-            <Switch
-              onChange={(val) => setSendMailValue(val)}
-            />
+            <Switch />
           </Form.Item>
           <Form.Item
-            label="Send mail after down count"
-            name="resetAfterDownCount"
-            hidden={!sendMailValue}
-            rules={[{
-              validator: async (_, value) => {
-                if (sendMailValue && typeof value !== 'number') {
-                  throw new Error('Reset After is required')
-                }
-              }
-            }]}
+            noStyle
+            shouldUpdate={(prev, curr) => {
+              return prev.sendMailOnFailure !== curr.sendMailOnFailure
+            }}
           >
-            <InputNumber min={1} />
+            {() => {
+              if (!form.getFieldValue('sendMailOnFailure')) {
+                form.setFieldsValue({
+                  ...form.getFieldsValue(),
+                  resetAfterDownCount: null
+                })
+              }
+              
+              return (
+                <Form.Item
+                  label="Send mail after down count"
+                  name="resetAfterDownCount"
+                  shouldUpdate
+                  hidden={!form.getFieldValue('sendMailOnFailure')}
+                  rules={[{
+                    validator: async (_, value) => {
+                      if (form.getFieldValue('sendMailOnFailure') && typeof value !== 'number') {
+                        throw new Error('Reset After is required')
+                      }
+                    }
+                  }]}
+                >
+                  <InputNumber min={1}/>
+                </Form.Item>
+              )
+            }}
           </Form.Item>
         </Form>
       </Wrapper>
