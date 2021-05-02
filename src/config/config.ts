47fc5e22a4e60d9427fb47e7852444ccd06a0956
gaurav <gauravkalyan17@gmail.com>
2021-05-02 21:27:21 +0530

    config updated

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,4 +1,15 @@
+// export const config = Object.freeze({
+//   backendBaseUrl: `http://localhost:9000/`,
+//   webSocketUrl: `ws://localhost:9000/graphql`,
+// })
+
+// export const config = Object.freeze({
+//   backendBaseUrl: `http://${window.location.host}/`,
+//   webSocketUrl: `ws://${window.location.host}/ws/`,
+// })
+
 export const config = Object.freeze({
-  backendBaseUrl: '/',
-  webSocketUrl: `ws://${window.location.host}/ws/`,
+  backendBaseUrl: `https://${window.location.host}/`,
+  webSocketUrl: `wss://${window.location.host}/ws/`,
 })
+
